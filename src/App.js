cbfe589c6d1672cc0a8b701e3c504ab620192bab
Ganesh Bhusi <ganeshpaul999@gmail.com>
2021-07-18 17:42:15 +0530

    new deply

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h2>Hey It's my New React Application</h2>
+        <h4>
+          G <span style={{color: "red"}}>❤️</span> S
+        </h4>
       </header>
     </div>
   );
